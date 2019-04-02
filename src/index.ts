83e204c672ad1d4cd822c2f9f7c0cd830c9dcdcb
shih_jun_teh <shih_jun_teh@dell.com>
2019-04-02 15:09:48 +0800

    initial page setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("haha");
+var radios = document.getElementsByName('displayS').value;
+
+for (var i = 0, length = radios.length; i < length; i++) {
+  if (radios[i].checked) {
+    // do whatever you want with the checked radio
+    document.getElementsByName('.display').innerHTML = radios[i].value
+
+    // only one radio can be logically checked, don't check the rest
+    break;
+  }
+}
+
+
+
